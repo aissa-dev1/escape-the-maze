05508dd95ca32c5935c48896e63da2e300420993
aissa-dev1 <aissadev1@gmail.com>
2025-01-16 23:57:17 +0100

    create a getter and setter for canvas and ctx props and create a set canvas method

--- src/game/board.ts
+++ src/game/board.ts
@@ -8,16 +8,16 @@ interface BoardConfig {
 }
 
 export class Board {
-  readonly canvas!: HTMLCanvasElement;
-  readonly ctx!: CanvasRenderingContext2D;
+  private _canvas!: HTMLCanvasElement;
+  private _ctx!: CanvasRenderingContext2D;
   private _width = 350;
   private _height = 350;
   readonly styles?: Partial<CSSStyleDeclaration> = undefined;
 
   constructor(config: BoardConfig) {
-    this.canvas = config.canvas;
-    if (this.canvas instanceof HTMLCanvasElement) {
-      this.ctx = this.canvas.getContext("2d")!;
+    this._canvas = config.canvas;
+    if (this._canvas instanceof HTMLCanvasElement) {
+      this._ctx = this._canvas.getContext("2d")!;
     }
     this._width = config.width || this._width;
     this._height = config.height || this._height;
@@ -25,9 +25,14 @@ export class Board {
   }
 
   init() {
-    this.canvas.width = this.width;
-    this.canvas.height = this.height;
-    createStyle(this.canvas, this.styles);
+    this._canvas.width = this.width;
+    this._canvas.height = this.height;
+    createStyle(this._canvas, this.styles);
+  }
+
+  setCanvas(canvas: HTMLCanvasElement) {
+    this._canvas = canvas;
+    this._ctx = canvas.getContext("2d")!;
   }
 
   get width(): number {
@@ -36,5 +41,13 @@ export class Board {
 
   get height(): number {
     return this._height;
+  }
+
+  get canvas(): HTMLCanvasElement {
+    return this._canvas;
+  }
+
+  get ctx(): CanvasRenderingContext2D {
+    return this._ctx;
   }
 }
